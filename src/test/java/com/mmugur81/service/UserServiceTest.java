db32c11b6a9df3068df0e23d463c58c1c8af9f96
Mugurel Mirica <mmugur81@gmail.com>
2016-09-09 16:17:50 +0300

    User Service Test - part 2

--- src/test/java/com/mmugur81/service/UserServiceTest.java
+++ src/test/java/com/mmugur81/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.mmugur81.service;
 
 import com.mmugur81.model.User;
+import com.mmugur81.model.UserRole;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -49,7 +51,7 @@ public class UserServiceTest {
 
     @Test
     public void registerUserTest() {
-        // Given - user already created in before
+        // Given - user already created in before()
 
         // When
         User savedUser = userService.registerByModel(user);
@@ -62,5 +64,72 @@ public class UserServiceTest {
 
         // and that password has been hashed
         assertThat(savedUser, hasProperty("password", not(equalTo(password))));
+    }
+
+    @Test
+    public void getSavedUserTest() {
+        // Given - user already created in before()
+        user.setStatus(User.Status.Pending);
+        User savedUser = userService.registerByModel(user);
+
+        // When
+        User loadedUser = userService.get(savedUser.getId());
+
+        // Then
+        Assert.assertEquals(savedUser.getFirstName(), loadedUser.getFirstName());
+        Assert.assertEquals(savedUser.getLastName(), loadedUser.getLastName());
+        Assert.assertEquals(savedUser.getEmail(), loadedUser.getEmail());
+        Assert.assertEquals(savedUser.getPassword(), loadedUser.getPassword());
+        Assert.assertEquals(savedUser.getStatus(), loadedUser.getStatus());
+    }
+
+    @Test
+    public void addUserRoleTest() {
+        // Given
+        user.addRole(UserRole.Role.USER);
+        user.addRole(UserRole.Role.ADMIN);
+
+        // When
+        User savedUser = userService.registerByModel(user);
+
+        // Then
+        assertThat(savedUser.getUserRoles(), hasSize(2));
+        Assert.assertTrue(savedUser.hasRole(UserRole.Role.USER));
+        Assert.assertTrue(savedUser.hasRole(UserRole.Role.ADMIN));
+    }
+
+    @Test
+    public void userHasDefaultRoleTest() {
+        // Given - user already created in before()
+
+        // When
+        User savedUser = userService.registerByModel(user);
+
+        // Then
+        Assert.assertTrue(savedUser.hasRole(UserRole.Role.USER));
+    }
+
+    @Test
+    public void userDoesntHaveRoleTest() {
+        // Given
+        user.addRole(UserRole.Role.USER);
+
+        // When
+        User savedUser = userService.registerByModel(user);
+
+        // Then
+        Assert.assertFalse(savedUser.hasRole(UserRole.Role.ADMIN));
+    }
+
+    @Test
+    public void disableUserTest() {
+        // Given - user already created in before()
+
+        // When
+        User disabledUser = userService.disable(user);
+
+        // Then
+        assertThat(disabledUser, hasProperty("status", equalTo(User.Status.Disabled)));
+        assertThat(disabledUser, hasProperty("email", not(equalTo(email))));
     }
 }
