60c8816c03800c7cacd221908a47648982dac9dc
Mugurel Mirica <mmugur81@gmail.com>
2016-09-25 10:29:35 +0300

    TESTING - update import.sql

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -4,11 +4,15 @@ VALUES
   (1,'2016-08-16 18:03:03','2016-08-16 18:03:03','ceva@nimic.lol','Pending',NULL,NULL,NULL),
   (2,'2016-09-14 20:33:02','2016-09-14 20:33:02','mmugur81@gmail.com','Active','Mugurel','Mirica','$2a$10$tpMq18uWC1kVsKFA3mwTLeVlZMfYUWoSiSmbY4Tf0wPwNdWk6GfPK');
 
-INSERT INTO user_roles VALUES
+INSERT INTO user_roles 
+  (id, created, updated, role, id_user)
+VALUES
   (1,'2016-09-14 20:33:02','2016-09-14 20:33:02','USER',2),
   (2,'2016-09-14 20:33:02','2016-09-14 20:33:02','ADMIN',2);
 
-INSERT INTO categories VALUES
+INSERT INTO categories
+  (id, created, updated, name, id_category_parent, description)
+VALUES
   (1,'2016-08-16 18:03:03','2016-08-16 18:03:03','Laptop-uri',NULL,NULL),
   (2,'2016-08-16 18:03:03','2016-08-16 18:03:03','Telefoane',NULL,NULL),
   (3,'2016-08-16 18:03:03','2016-08-16 18:03:03','Samsung',2,NULL),
@@ -25,6 +29,12 @@ VALUES
   (2,'2016-09-14 20:45:55','2016-09-14 20:45:55','S2 1GB'          ,'','RON',2000,6,2),
   (3,'2016-09-14 20:47:19','2016-09-14 20:47:19','Gaming Alien LPT','','EUR',2500,1,2);
 
-INSERT INTO orders VALUES (1,'2016-09-14 20:53:14','2016-09-14 20:53:14',NULL,NULL,false,'Pending','RON',3500,2);
+INSERT INTO orders
+  (id, created, updated, notes, pay_date, payed, status, currency, price, id_user)
+VALUES
+  (1,'2016-09-14 20:53:14','2016-09-14 20:53:14',NULL,NULL,false,'Pending','RON',3500,2);
 
-INSERT INTO order_items VALUES (1,1,'RON',1500,1,1),(2,2,'RON',2000,1,2);
+INSERT INTO order_items
+  (id,item_number, currency, price, id_order, id_product)
+VALUES
+  (1,1,'RON',1500,1,1),(2,2,'RON',2000,1,2);
