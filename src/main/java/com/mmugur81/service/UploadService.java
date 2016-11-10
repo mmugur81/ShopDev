cdd04fdfb1c870f25fbe79d0ea44a8eabf240dc2
Mugurel Mirica <mmugur81@gmail.com>
2016-11-10 18:31:52 +0200

    Added UploadService

---
+++ src/main/java/com/mmugur81/service/UploadService.java
@@ -0,0 +1,34 @@
+package com.mmugur81.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by mugurel on 10.11.2016.
+ * Service for handling file uploads (usually images)
+ */
+
+public interface UploadService {
+
+    enum Target {
+        PRODUCT
+    }
+
+    enum Type {
+        IMAGE
+    }
+
+    boolean uploadFile(MultipartFile uploadedFile, long entityId, Target target, Type fileType);
+
+    /**
+     * Target file name format: "{Target}-{id}.{extension}"
+     *
+     * @param target target type
+     * @param entityId id
+     * @return String
+     */
+    String retrieveFileName(Target target, long entityId);
+
+    default String getFileNameFormat(Target target, long entityId) {
+        return target + "-" + entityId;
+    }
+}
---
+++ src/main/java/com/mmugur81/service/UploadServiceImpl.java
@@ -0,0 +1,78 @@
+package com.mmugur81.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by mugurel on 10.11.2016.
+ * uploadService implementation
+ */
+
+@Service
+public class UploadServiceImpl implements UploadService {
+
+    private final String uploadDirName = "uploads";
+
+    private String uploadDir;
+
+    public UploadServiceImpl() {
+        // Create upload directory if necessary
+        uploadDir = System.getProperty("user.dir") + "/" + uploadDirName + "/";
+        if (!Files.exists(Paths.get(uploadDir))) {
+            try {
+                Files.createDirectory(Paths.get(uploadDir));
+            }
+            catch (IOException e) {
+                uploadDir = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean uploadFile(MultipartFile uploadedFile, long entityId, Target target, Type fileType) {
+
+        String extension = uploadedFile.getOriginalFilename().split("\\.")[1];
+        String newFileName = this.getFileNameFormat(target, entityId) + "." + extension;
+
+        try {
+            // Delete old file
+            String oldFileName = this.retrieveFileName(target, entityId);
+            if (oldFileName != null) {
+                new File(uploadDir + oldFileName).delete();
+            }
+
+            File file = new File(uploadDir + newFileName);
+            uploadedFile.transferTo(file);
+        }
+        catch (IOException e) {
+            // Do some file error handling
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String retrieveFileName(Target target, long entityId) {
+        // Scan for a file matching the correct format
+        String fileNamePrefix = this.getFileNameFormat(target, entityId);
+
+        File dir = new File(uploadDir);
+        File[] matches = dir.listFiles((file, s) -> s.startsWith(fileNamePrefix+"."));
+
+        if (matches.length > 0) {
+            return matches[0].getName();
+        }
+
+        return null;
+    }
+}
